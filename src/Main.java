07dc94b12b741d74ae3d0476eb2509c165c74316
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2021-12-23 14:25:54 +0300

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int priceTicket = 5000;
         int amount = 20;
         int bonus = 1;
-        int countMiles = priceTicket*bonus/amount;
+        int countMiles = priceTicket * bonus / amount;
         System.out.println("Количество начисленных милей:" + countMiles);
-
     }
 }
